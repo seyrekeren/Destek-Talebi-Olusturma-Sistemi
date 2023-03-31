317293f7b3da416d9f3cc4db1991292d66b3a176
Eren seyrek <erenseyrek@Erens-MacBook-Air.local>
2023-03-31 22:27:45 +0300

    final

--- Models/Ticket.js
+++ Models/Ticket.js
@@ -3,13 +3,13 @@ Schema = mongoose.Schema;
 
 const TicketSchema = new mongoose.Schema({
     department: { type: mongoose.Schema.Types.ObjectId, ref: 'Department', required: true },
-    createdBy: { type: String, required: true },
-    assignedTo: { type: String },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref:'User', required: true },
+    assignedTo: { type: mongoose.Schema.Types.ObjectId, ref:'User' },
     status: { type: String, enum: ['open', 'in_progress', 'closed'], default: 'open' },
     priority: { type: String, enum: ['low', 'medium', 'high'], default: 'low' },
     description: { type: String, required: true },
     comments: [{
-        createdBy: { type: String,  required: true },//ref user objectıd
+        createdBy: { type:mongoose.Schema.Types.ObjectId, ref:'User',  required: true },//ref user objectıd
         message: { type: String, required: true },
         createdAt: { type: Date, default: Date.now }
     }],
